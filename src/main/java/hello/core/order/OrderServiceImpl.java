0954bdb4c26f24dc2f3883d49ff17502b331f043
황효주 <hjoo830@naver.com>
2026-01-03 22:47:16 +0900

    인터페이스에만 의존하도록 관심사 분리

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,22 +7,13 @@ import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
 
 public class OrderServiceImpl implements OrderService {
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+    private DiscountPolicy discountPolicy;
 
-    /**
-     * 추상(인터페이스) 뿐만 아니라 구체(구현) 클래스에도 의존하고 있다.
-     * 추상(인터페이스) 의존: `DiscountPolicy`
-     * 구체(구현) 클래스: `FixDiscountPolicy` , `RateDiscountPolicy`
-     */
-
-    /**
-     * `FixDiscountPolicy` 를 `RateDiscountPolicy` 로 변경하는 순간
-     * `OrderServiceImpl` 의 소스 코드도 함께 변경해야 한다!
-     * -> OCP 위반
-     */
-
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
-    private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
